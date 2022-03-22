7eac78e85ee7fb16c46ad2e14d761ddb62d69c4b
Victor Hugo da Silva <76711024+fvictoor@users.noreply.github.com>
2022-03-22 13:35:51 -0300

    Update Texto_to_audio.py

--- Texto_to_audio.py
+++ Texto_to_audio.py
@@ -1,79 +1,69 @@
-
-from email import message
-from tkinter import font
-from gtts import gTTS
-from tkinter import messagebox
 from tkinter import *
-import customtkinter
-
-radius_defaut = 5
-# C:\Users\fvict\appdata\roaming\python\python310\site-packages\customtkinter
-  # Themes: "blue" (standard), "green", "dark-blue"
+from tkinter import messagebox
+from gtts import gTTS
+from pkg_resources import resource_filename
 
 def limpa_tela():
-    text_entry.delete(0, END)
+    entry0.delete(0, END)
 
 def btn_clicked():
     idioma = 'pt'
-    texto = text_entry.get()
+    texto = entry0.get()
     audio = gTTS(text=texto, lang=idioma,tld='com.br')
     audio.save('audio.mp3')
     messagebox.showinfo(message="Áudio gerado com sucesso!")
     limpa_tela()
 
-window = customtkinter.CTk()
+window = Tk()
 window.geometry("600x450")
+window.configure(bg = "#2e2e2e")
+icon_img = resource_filename(__name__,r"ico.ico")
 window.title("Conversor de Texto para Áudio")
-customtkinter.set_appearance_mode("Dark")
-customtkinter.set_default_color_theme("blue")
+window.iconbitmap(False, icon_img)
+canvas = Canvas(
+    window,
+    bg = "#2e2e2e",
+    height = 450,
+    width = 600,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
 
+background_imp = resource_filename(__name__,r"background.png" )
+background_img = PhotoImage(file = background_imp)
+background = canvas.create_image(
+    299.5, 100.0,
+    image=background_img)
+entry0_imp = resource_filename(__name__,r"img_textBox0.png" )
+entry0_img = PhotoImage(file = entry0_imp)
+entry0_bg = canvas.create_image(
+    299.5, 224.5,
+    image = entry0_img)
 
-Titulo = customtkinter.CTkLabel(master=window,
-                              text="Converter Texto em Áudio",
-                              width=100,
-                              height=50,
-                              corner_radius=1,
-                              text_font= ("Roboto",'25','bold')
-                              )
-Titulo.place(relx=0.19,
-           rely=0.1,
-           )
+entry0 = Entry(
+    bd = 0,
+    bg = "#ffffff",
+    highlightthickness = 0)
 
-text = customtkinter.CTkLabel(master=window,
-                              text="Insira o texto aqui",
-                              width=100,
-                              
-                              corner_radius=1,
-                              text_font= ("Roboto",'12','italic')
-                              )
-text.place(relx=0.39,
-           rely=0.28,
-           )
-
-text_entry = customtkinter.CTkEntry(master=window,
-                                    width=361,
-                                    height=143,
-                                    corner_radius=0,
-                                    border_width=1,
-                                    fg_color="#1673a1"
-                                                                       )
-text_entry.place(x = 119, y = 152,
+entry0.place(
+    x = 119, y = 152,
     width = 361,
     height = 143)
 
+entry_img_imp = resource_filename(__name__,r"img0.png")
+img0 = PhotoImage(file = entry_img_imp)
+b0 = Button(
+    image = img0,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
 
-
-btn_Consult = customtkinter.CTkButton(master=window,
-                                      text='Converter',
-                                      corner_radius=radius_defaut,
-                                      bg_color=("#b9dbe8"),
-                                      command=btn_clicked,
-                                      relief = "flat",
-                                      highlightthickness = 0)
-btn_Consult.place(
+b0.place(
     x = 226, y = 335,
     width = 147,
-    height = 33)
+    height = 34)
 
 window.resizable(False, False)
 window.mainloop()
